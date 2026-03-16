46ea9a949063215ec4082f020ff24b63f7648816
Victor Hugo <victorvilto@gmail.com>
2026-03-15 22:52:52 -0300

    Começando a configurar o Swagger

--- src/main/java/com/devvictorh/cashflow/config/SecurityConfig.java
+++ src/main/java/com/devvictorh/cashflow/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll()
                         .requestMatchers("/api/users/**").hasRole("ADMIN")
                         .requestMatchers("/api/categories/**").authenticated()
                         .requestMatchers("/api/income/**").authenticated()
